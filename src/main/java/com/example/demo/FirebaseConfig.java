fa2916bde46051db963c9b708794e2c7e3f71d69
Barani57 <baranimc888@gmail.com>
2024-12-30 04:50:26 +0530

    Update FirebaseConfig to use relative path for serviceAccountKey.json

--- src/main/java/com/example/demo/FirebaseConfig.java
+++ src/main/java/com/example/demo/FirebaseConfig.java
@@ -1,14 +1,10 @@
 package com.example.demo;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import javax.annotation.PostConstruct;
-
-import java.io.File;
 import java.io.FileInputStream;
 
 
@@ -17,8 +13,7 @@ public class FirebaseConfig {
 	 @PostConstruct
 	    public void initialize() {
 	        try {
-	        	 File file = new File("/app/serviceAccountKey.json");
-	             FileInputStream serviceAccount = new FileInputStream(file);
+	        	 FileInputStream serviceAccount = new FileInputStream(System.getenv("GOOGLE_APPLICATION_CREDENTIALS"));
 	            
 	            FirebaseOptions options = FirebaseOptions.builder()
 	                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
