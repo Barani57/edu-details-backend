f9c7cfd5e1812b0d4acf1dbfa0720be116db586e
Barani57 <baranimc888@gmail.com>
2024-12-30 13:21:23 +0530

    Fixed Firebase configuration and Dockerfile

--- src/main/java/com/example/demo/FirebaseConfig.java
+++ src/main/java/com/example/demo/FirebaseConfig.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -16,10 +18,21 @@ import java.nio.charset.StandardCharsets;
 public class FirebaseConfig {
 	 @PostConstruct
 	    public void initialize() {
-		 try (InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json")) {
-	            if (serviceAccount == null) {
-	                throw new IllegalArgumentException("serviceAccountKey.json not found in resources");
+		 try {
+			  String credPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+	            InputStream serviceAccount;
+	            
+	            if (credPath != null) {
+	                serviceAccount = new ClassPathResource(credPath).getInputStream();
+	            } else {
+	                // Fallback to classpath resource
+	                serviceAccount = getClass().getResourceAsStream("/serviceAccountKey.json");
 	            }
+	            
+	            if (serviceAccount == null) {
+	                throw new IllegalArgumentException("Firebase configuration not found");
+	            }
+
 
 	            FirebaseOptions options = FirebaseOptions.builder()
 	                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
